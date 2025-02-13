d7a89ef64303ba62b30d41c61392c3f38970bb6d
David Koy <dzilla398@gmail.com>
2025-02-13 11:44:40 +1030

    Modified the navbar to contain buttons instead of links in html file. Created a `class MainPg` that had method `showMainPgContent` which contains the logic for appending text and headers for description of restuarant pg

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import "./styles.css"
-console.log("Hello")
+/**
+ * Main Page will have the following
+ * 1. Small description of restuarnt
+ * 2. Contact Hours
+ * 3. Location of Address
+ * 4. Load Images 
+ */
+class MainPg
+{
+    constructor()
+    {
+        this.mainPgContent = document.querySelector(".content")
+        this.header1 = document.createElement("h1")
+        this.descriptionPara = document.createElement("p")
+    }
+
+    showMainPgContent()
+    {
+        // Create Header
+        this.header1.textContent = "Welcome to AU Cafe Brekkie"
+        this.mainPgContent.appendChild(this.header1)
+
+        // Create a Description
+        this.descriptionPara.textContent = "Here we offer classical Australian Cafe experience where you can enjoy delicious homemade roasting coffee with delicious food that will make your stomach happy"
+        this.mainPgContent.appendChild(this.descriptionPara)
+    }
+}
+
+const main = new MainPg()
+main.showMainPgContent()
