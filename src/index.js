f9d2e2d581e710f0e1303a374bafb0237d47a8ba
David Koy <dzilla398@gmail.com>
2025-02-25 12:41:58 +1030

    Added the asset to import images in js in the `webpack.config.js`. Commented out the `.img` and `.content` in the css file.
    Modified the `index.js` to import images and styled the main page of the restaurnt. Removed the `styleMainPg()` and refactored all the styling into `showMainPgContent`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css"
 import { menu } from "./menu.js"
 import { contacts } from "./contact.js"
+import cafeImg from "./pics/louis-hansel-qoPAjwEiUmg-unsplash.jpg"
+
 
 class MainPg
 {
@@ -11,31 +13,39 @@ class MainPg
         this.descriptionPara = document.createElement("p")
         this.buttons = document.querySelectorAll("button")
         this.divForMain = document.createElement("div")
+        this.createImgDiv = document.createElement("div")
+        // this.imageBackground = document.createElement("img")
         this.divForMain.className = "main-div"
-        this.backgroundImg = document.querySelector(".img")
     }
 
     showMainContent()
-    {      
+    {    
         this.divForMain.textContent = ""
-
-        // Create Header
+        
+        // styling the main-div 
+        this.divForMain.style.gridRow = " 1 / 2"
+        this.divForMain.style.gridColumn = " 2 / 3"
+        
+        // creating and style the Header
         this.header1.textContent = "Odin Brekkie"
         this.header1.style.fontSize = "4rem"
-        // this.header1.style.textAlign = "center"
+        this.header1.style.gridColumn = " 2 / 3"
+        this.header1.style.gridRow = "1 / 2"
+        this.header1.style.textAlign = "center"
         this.header1.style.textShadow = "0.5rem 0.5rem 1rem black"
         this.header1.style.color = "white"
         this.mainPgContent.appendChild(this.header1)
-        this.mainPgContent.appendChild(this.divForMain)
-    }
 
-    styleMainPg()
-    {
-        this.divForMain.style.display = "grid"
-        this.divForMain.style.gridRow = " 2 / 3"
-        this.divForMain.style.gridColumn = " 2 / 3"
-        this.header1.style.gridRow = "1/3"
-        this.header1.style.gridColumn = "2/3"
+        // create and style img
+        this.createImgDiv.className = "img"
+        this.createImgDiv.style.backgroundImage = `url(${cafeImg})`
+        this.createImgDiv.style.backgroundSize = "100% 100%"
+        this.createImgDiv.style.backgroundRepeat = "no-repeat"
+        this.createImgDiv.style.gridColumn = "2/3"
+        this.createImgDiv.style.gridRow = "2/3"
+        
+        this.mainPgContent.appendChild(this.createImgDiv)
+        this.mainPgContent.appendChild(this.divForMain)
     }
 
     switchTabs()
@@ -48,7 +58,7 @@ class MainPg
                 {
                     menu.divForMenu.style.display = "none"
                     contacts.divForContacts.style.display = "none"
-                    this.styleMainPg()
+                    this.divForMain.style.display = "grid"
                     this.showMainContent()
                 }
                 else if(btn.textContent === "Menu")
@@ -72,7 +82,6 @@ class MainPg
             })
         })
     }
-
 }
 
 
